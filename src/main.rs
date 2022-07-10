08c1c7f3c5ccb63d455a485ea6e54b4ba076b660
Ayrat Badykov <ayratin555@gmail.com>
2022-07-10 20:39:39 +0300

    experiment with positions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
+use rand::seq::SliceRandom;
 
-const CARD: &str = "four.png";
-const SPRITE_SCALE: f32 = 0.5;
+const SPRITE_SCALE: f32 = 0.2;
 
 fn main() {
     App::new()
@@ -18,17 +18,52 @@ fn main() {
         .run();
 }
 
-fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    window: Res<WindowDescriptor>,
+) {
     // camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
-    // add card
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load(CARD),
-        transform: Transform {
-            scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
+    let cards: Vec<HandleUntyped> = asset_server.load_folder("cards").unwrap();
+    let tile_size = tile_size(window, cards.len());
+
+    let board_size = (5.0, 5.0);
+
+    let board_position = Vec3::new(-(board_size.0 / 2.), -(board_size.1 / 2.), 0.);
+
+    commands
+        .spawn()
+        .insert(Transform::from_translation(board_position))
+        .insert(GlobalTransform::default())
+        .with_children(|parent| {
+            for x in 1..(board_size.0 as u16) {
+                for y in 1..(board_size.1 as u16) {
+                    let random_card = cards
+                        .choose(&mut rand::thread_rng())
+                        .unwrap()
+                        .clone()
+                        .typed();
+                    // add card
+                    parent.spawn_bundle(SpriteBundle {
+                        texture: random_card,
+                        transform: Transform {
+                            scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+                            translation: Vec3::new(
+                                (x as f32 * tile_size) - (tile_size / 2.),
+                                (y as f32 * tile_size) - (tile_size / 2.),
+                                1.,
+                            ),
+                            ..Default::default()
+                        },
+                        ..Default::default()
+                    });
+                }
+            }
+        });
+}
+
+fn tile_size(window: Res<WindowDescriptor>, card_count: usize) -> f32 {
+    window.width / card_count as f32
 }
